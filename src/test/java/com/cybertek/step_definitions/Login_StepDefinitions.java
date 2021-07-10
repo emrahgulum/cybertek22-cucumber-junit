a7ef2365fc967e0869f4035d7f9cad1d4bc85510
emrahgulum <82914905+emrahgulum@users.noreply.github.com>
2021-07-10 17:01:14 -0400

    added new step definitions file

--- src/test/java/com/cybertek/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/Login_StepDefinitions.java
@@ -1,5 +1,6 @@
 package com.cybertek.step_definitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -7,23 +8,39 @@ import io.cucumber.java.en.When;
 public class Login_StepDefinitions {
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User is on the login page");
     }
     @When("user enters librarian username")
     public void user_enters_librarian_username() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User enters librarian username");
     }
     @When("user enters librarian password")
     public void user_enters_librarian_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User enters librarian password");
     }
     @Then("user should see the dashboard")
     public void user_should_see_the_dashboard() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User sees the dashboard");
     }
 
+    @When("user enters student username")
+    public void userEntersStudentUsername() {
+        System.out.println("User enters student username");
+
+    }
+
+    @And("user enters student password")
+    public void userEntersStudentPassword() {
+        System.out.println("User enters student password");
+    }
+
+    @When("user enters admin username")
+    public void userEntersAdminUsername() {
+        System.out.println("User enters admin username");
+    }
+
+    @And("user enters admin password")
+    public void userEntersAdminPassword() {
+        System.out.println("User enters admin password");
+    }
 }
